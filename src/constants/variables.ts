98a79af8f7fd2ae7a59a94f0831359bf780bec38
Athirson Silva <Athirsonarceus@gmail.com>
2023-09-18 18:47:17 -0300

    pleeeeeease

--- src/constants/variables.ts
+++ src/constants/variables.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL = "https://jchat-server.up.railway.app"
-// export const SERVER_URL = "http://localhost:8080"
+// export const SERVER_URL = "https://jchat-server.up.railway.app"
+export const SERVER_URL = "http://localhost:8080"
